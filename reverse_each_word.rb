b2c93db2906c044070cc49a0becbe91ffe42fb95
Stacy Johns <stacykjohns@icloud.com>
2019-07-20 14:59:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,3 @@
-def reverse_each_word(string)
-  array = string.split
-  new_string = ""
-  array.each do |array|
-    new_string << array.reverse
-  end
-  new_string.join(' ')
-end
 
 def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
