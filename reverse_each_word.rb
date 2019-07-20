828c2b55e5496794e543030d0348153bd05bf3bf
Stacy Johns <stacykjohns@icloud.com>
2019-07-20 14:37:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(string)
-  
+  string = string.split("")
+  reversed_string = ""
+  string.each do |character|
+    reversed_string.unshift(character)
+  end
+  return reversed_string.join
 end
